41442530f4d6e2a61709e896bc95b569b18c5c4a
Samuel Cabrero <scabrero@zentyal.com>
2013-07-30 13:26:43 +0200

    Initial version of the LoadParm module

--- src/LoadParm/lib/Samba/LoadParm.pm
+++ src/LoadParm/lib/Samba/LoadParm.pm
@@ -34,53 +34,104 @@ XSLoader::load('Samba::LoadParm', $VERSION);
 
 1;
 __END__
-# Below is stub documentation for your module. You'd better edit it!
 
 =head1 NAME
 
-Samba::LoadParm - Perl extension for blah blah blah
+Samba::LoadParm - Extension for parsing and writting Samba configuration files
 
 =head1 SYNOPSIS
 
   use Samba::LoadParm;
-  blah blah blah
+
+  my $lp = new Samba::LoadParm();
+  $lp->load_default();
 
 =head1 DESCRIPTION
 
-Stub documentation for Samba::LoadParm, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
+  This module uses an object interface
 
-Blah blah blah.
+=over
 
-=head2 EXPORT
+=item new
+
+  Instantiate object.
+
+=item load_default
+
+  Load default smb.conf file. Returns 1 on success.
+
+=item load filename
+
+  Load specified file. Returns 1 on success.
+
+=item is_myname name
+
+  Check whether the specified name matches one of our netbios names.
+
+=item is_mydomain name
+
+  Check whether the specified name matches our domain name.
+
+=item private_path name
+
+  Returns an absolute path to a file in the Samba private directory.
+
+=item server_role
+
+  Get the server role.
+
+=item default_path
+
+  Returns the default smb.conf path.
+
+=item setup_dir
+
+  Returns the compiled in location for provision tempates.
+
+=item modules_dir
+
+  Returns the compiled in location of modules.
+
+=item bin_dir
+
+  Returns the compiled in BINDIR.
+
+=item sbin_dir
+
+  Returns the compiled in SBINDIR.
+
+=back
+
+=head1 EXPORT
 
 None by default.
 
-
-
 =head1 SEE ALSO
 
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
-
-If you have a mailing list set up for your module, mention it here.
-
-If you have a web site set up for your module, mention it here.
+Samba::Credentials
+Samba::Smb
+Samba::Security::Descriptor
 
 =head1 AUTHOR
 
-zen, E<lt>zen@E<gt>
+Samuel Cabrero, E<lt>scabrero@zentyal.comE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2013 by zen
+Copyright (C) 2013 by Zentyal S.L.
 
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself, either Perl version 5.14.2 or,
-at your option, any later version of Perl 5 you may have available.
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 2, as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 
 =cut
